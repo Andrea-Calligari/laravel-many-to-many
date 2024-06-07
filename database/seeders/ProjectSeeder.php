6efc67261664e1a96e2efeb22541a0c06ee8cbd8
Andrea-Calligari <andrea.calligari26@gmail.com>
2024-06-07 17:13:20 +0200

    ADDED: attach method on projectSeeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use App\Models\Type;
 use App\Models\Project;
 use Faker\Generator as Faker;
@@ -19,19 +20,34 @@ class ProjectSeeder extends Seeder
         $types = Type::all();
         $ids = $types->pluck('id')->all();
 
+        $technologies = Technology::all();
+        $tech_ids = $technologies->pluck('id')->all();
 
         for ($i = 0; $i < 10; $i++) {
 
             $new_project = new Project();
             // $new_project->type_id = Type::inRandomOrder()->first()->id;
             $new_project->type_id = $faker->optional()->randomElement($ids);
+           
+
             $project_name = $faker->sentence(6);
             $new_project->project_name = $project_name;
             $new_project->slug = Str::slug($project_name);
             $new_project->description = $faker->sentence(5);
             $new_project->working_hours = $faker->randomDigitNot(0);
             $new_project->co_workers = $faker->name();
+
+
+            // non ha ancora un id il progetto
             $new_project->save();
+            
+            
+            // prendo un numero random di id di technologies
+            $random_tech_ids = $faker->randomElements($tech_ids);
+            
+            // qui éstato salvato gli viene ora assegnato un id
+            $new_project->technologies()->attach($random_tech_ids);
+    
         }
 
     }
